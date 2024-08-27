95975eb60ea8665923102f31f667f8510997cb9c
Simon Pahl <simpahl@gmail.com>
2024-08-27 13:25:29 +0000

    fix linting issues and update tools

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 OTTO GmbH & Co KG
+// Copyright (c) 2024 OTTO GmbH & Co KG
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,14 +15,14 @@
 package utils
 
 import (
-	"io/ioutil"
+	"os"
 
 	"github.com/ghodss/yaml"
 )
 
 // Reads a YAML file and converts it to JSON blob
 func ReadYamlFileAsJson(filename string) ([]byte, error) {
-	content, err := ioutil.ReadFile(filename)
+	content, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
--- pkg/validation/validation.go
+++ pkg/validation/validation.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 OTTO GmbH & Co KG
+// Copyright (c) 2024 OTTO GmbH & Co KG
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@ package validation
 import (
 	"errors"
 	"fmt"
-	"os"
 	"log"
+	"os"
 	"path"
 
 	"github.com/xeipuuv/gojsonschema"
